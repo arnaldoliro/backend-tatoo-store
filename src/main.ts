b14422a4b6d857df90a79858dddb1ff351cb5393
Arnaldo Liro <dinholiro@gmail.com>
2025-08-13 14:03:13 -0300

    feat: creating registry dto and deploying swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+    .setTitle('Meu Projeto')
+    .setDescription('API do meu projeto NestJS')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap()
+  .then(() => {
+    console.log(
+      `🤖 Servidor rodando em: http://localhost:${process.env.PORT ?? 3000}`,
+    );
+    console.log(`📖 Documentação do Swagger Rodando em:  http://localhost:${process.env.PORT ?? 3000}/api`)
+  })
+  .catch((error) => {
+    console.error('Erro ao iniciar o servidor:', error);
+    process.exit(1);
+  });
