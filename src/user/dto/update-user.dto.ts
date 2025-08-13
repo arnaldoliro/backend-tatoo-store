9714c387f19604bb32b6e3887c6bf79666d065f5
Arnaldo Liro <dinholiro@gmail.com>
2025-08-13 11:14:51 -0300

    feat: configuring module and user dtos

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,5 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
-
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+import { IsOptional } from 'class-validator';
+export class UpdateUserDto {
+  @IsOptional()
+  name?: string;
+}
